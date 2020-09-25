c0ab36133480a5d323d30585ff9149e065717c8b
raevsanton <raevsanton@gmail.com>
2020-09-25 22:53:02 +0400

    add mock data

--- src/store/store.ts
+++ src/store/store.ts
@@ -6,36 +6,34 @@ class Store {
     @observable error: boolean = false;
     @observable isFetching: boolean = true;
 
-    @action.bound
+    @action
     setTickets = () => {
-        fetch('https://front-test.beta.aviasales.ru/search')
+        fetch('./tickets.json')
             .then(res => res.json())
-            .then(data => fetch(`https://front-test.beta.aviasales.ru/tickets?searchId=${data.searchId}`))
-                .then(res => res.json())
-                .then(data => {
-                    this.ticketsData = data.tickets
-                        .slice(0, 5)
-                        .map((ticket: Ticket) => {
-                            const object: TicketDestruct = {
-                                price: ticket.price,
-                                carrier: ticket.carrier,
-                                originFrom: ticket.segments[0].origin,
-                                destinationFrom: ticket.segments[0].destination,
-                                dateFrom: ticket.segments[0].date,
-                                stopsFrom: ticket.segments[0].stops,
-                                durationFrom: ticket.segments[0].duration,
-                                originTo: ticket.segments[1].origin,
-                                destinationTo: ticket.segments[1].destination,
-                                dateTo: ticket.segments[1].date,
-                                stopsTo: ticket.segments[1].stops,
-                                durationTo: ticket.segments[1].duration
-                            };
-                            return object;
-                        })
-                        .sort((ticketOne: TicketDestruct, ticketTwo: TicketDestruct) => ticketOne.price - ticketTwo.price);
-                    this.isFetching = false;
-                })
-            .catch(error => this.error = true)
+            .then(data => {
+                this.ticketsData = data[0].tickets
+                    .slice(0, 5)
+                    .map((ticket: Ticket) => {
+                        const object: TicketDestruct = {
+                            price: ticket.price,
+                            carrier: ticket.carrier,
+                            originFrom: ticket.segments[0].origin,
+                            destinationFrom: ticket.segments[0].destination,
+                            dateFrom: ticket.segments[0].date,
+                            stopsFrom: ticket.segments[0].stops,
+                            durationFrom: ticket.segments[0].duration,
+                            originTo: ticket.segments[1].origin,
+                            destinationTo: ticket.segments[1].destination,
+                            dateTo: ticket.segments[1].date,
+                            stopsTo: ticket.segments[1].stops,
+                            durationTo: ticket.segments[1].duration
+                        };
+                        return object;
+                    })
+                    .sort((ticketOne: TicketDestruct, ticketTwo: TicketDestruct) => ticketOne.price - ticketTwo.price);
+                this.isFetching = false;
+            })
+        .catch(error => this.error = true)
     }
 
     @computed
